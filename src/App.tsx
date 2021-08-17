64de9a2a7257ae8bf1c904be61924d427189f15e
Aleck Landgraf <aleck.landgraf@airbnb.com>
2021-08-16 21:39:34 -0700

    build a deck diff

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,85 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+
+type Deck = { name: string; qty: number }[];
+
+const COLS = 30;
+const ROWS = 10;
+
+function difference(setA: Set<string>, setB: Set<string>) {
+  let _difference = new Set(setA);
+  for (let elem of setB) {
+    _difference.delete(elem);
+  }
+  return _difference;
+}
 
 function App() {
+  const [deck1, setDeck1] = useState<string>("");
+  const [deck2, setDeck2] = useState<string>("");
+  const [diff, setDiff] = useState<string>("the diff will be generated here");
+
+  function handleSwap() {
+    setDeck1(deck2);
+    setDeck2(deck1);
+  }
+
+  function createDiff(a: string, b: string) {
+    if (a.length === 0 || b.length === 0) {
+      return;
+    }
+
+    const diff = difference(new Set(a.split("\n")), new Set(b.split("\n")));
+    setDiff([...diff].join("\n"));
+  }
+
+  function handleChange(value: string, deck: 1 | 2) {
+    if (deck === 1) {
+      setDeck1(value);
+      createDiff(value, deck2);
+    } else {
+      setDeck2(value);
+      createDiff(deck1, value);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="lists">
+        <div className="list">
+          <textarea
+            name="deck1"
+            id="deck1"
+            cols={COLS}
+            rows={ROWS}
+            value={deck1}
+            onChange={(event) => handleChange(event.target.value, 1)}
+          />
+        </div>
+        <button className="swap" onClick={handleSwap}>
+          🔄
+        </button>
+        <div className="list">
+          <textarea
+            name="deck2"
+            id="deck2"
+            cols={COLS}
+            rows={ROWS}
+            value={deck2}
+            onChange={(event) => handleChange(event.target.value, 2)}
+          />
+        </div>
+      </div>
+      <div className="diff">
+        <textarea
+          name="diff"
+          id="diff"
+          cols={COLS}
+          rows={ROWS}
+          value={diff}
+          readOnly
+        />
+      </div>
     </div>
   );
 }
